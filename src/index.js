3cfcfb6aa540eed745cbe0e717d4c7d6d3aa0b3a
joiq <frozeefrozee12345@gmail.com>
2021-02-13 22:22:35 +0300

    fix: correct infinity loop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 !== 0) {
+  if (str.length % 2 !== 0 || str == undefined || bracketsConfig == undefined) {
     return false;
   }
 
-  while (str.length != 0) {
+  let exitLoop = true;
+
+  while (str.length != 0 && exitLoop) {
+    exitLoop = false;
 
     for (let elem of bracketsConfig) {
       let pair = elem.join('');
@@ -11,6 +14,7 @@ module.exports = function check(str, bracketsConfig) {
       if (str.includes(pair)) {
         str = str.replace(pair, '');
         console.log(str);
+        exitLoop = true;
       }
     }
   }
